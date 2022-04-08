0659d324fc0838071c7109597db9b0d20302ec42
amit token <amitbabhanchak@gmail.com>
2022-04-08 19:45:14 +0530

    mya

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,18 +3,66 @@ const logger = require('./logger')
 
 const router = express.Router();
 
-router.get('/user-profile/:abcd', function(req, res) {
-    console.log(req)
-    console.log(req.params.abcd)
-    res.send('dummy response')
-})
+router.get('/GET/movies', function(req, res) {
+  //1 problem  
+    let movies = ['sole','karan arjun','dhamal','the kashmir file']
+    
+    res.send(movies)
+});
+router.get('/GET/movies/:indexNumber', function(req, res) {
+  //2 problem  
+    let movies = ['sole','karan arjun','dhamal','the kashmir file']
+    let id = req.params.indexNumber
+     if(id < movies.length) {
+         res.send(movies[id -1])
+     } else {
+// 3 problem
+     res.send('Use a valid index')    
+     }
+});
+//4 problem
+router.get('/GET/films', function (req, res) {
 
-router.get('/test-me', function (req, res) {
-    console.log('------------------')
-    console.log(req)
-    console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
-    res.send('My first ever api!')
+    let mv = [ {
+        "id": 1,
+        "name": 'The Shining'
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+       
+    res.send(mv)
+});
+//5 problem
+router.get('/GET/films/:filmld', function (req, res) {
+
+  let mvlist = [ {
+      "id": 1,
+      "name": 'The Shining'
+     }, {
+      "id": 2,
+      "name": "Incendies"
+     }, {
+      "id": 3,
+      "name": "Rang de Basanti"
+     }, {
+      "id": 4,
+      "name": "Finding Nemo"
+     }]
+     let filmId = req.params.filmld
+
+     if (filmId<mvlist.length){
+         res.send(mvlist[filmId-1])
+     } else {
+         res.send('No movie exists with this id')
+     }
+
 });
 
 
